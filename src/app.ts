eb7c540ecbccefcb5e69970742e29a753a82ff19
yakubsiyam <itsyakubsiyam@gmail.com>
2023-06-02 17:58:42 +0600

    Using semicolon

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import usersRouter from './app/modules/users/users.route'
-const app: Application = express()
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import usersRouter from './app/modules/users/users.route';
+const app: Application = express();
 
-app.use(cors())
+app.use(cors());
 
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application route
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', usersRouter);
 
 // Testing
 app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully')
-})
+  res.send('Working Successfully');
+});
 
-export default app
+export default app;

bf828e5e06e0eb0a533b734621bed83422a555f1
yakubsiyam <itsyakubsiyam@gmail.com>
2023-06-13 12:50:43 +0600

    [UPDATE] create user

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,18 +7,16 @@ import { generateUserId } from './user.utils';
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
   const id = await generateUserId();
-
   user.id = id;
   // default password
-
   if (!user.password) {
     user.password = config.default_user_pass as string;
   }
 
   const createdUser = await User.create(user);
 
-  if (!createUser) {
-    throw new ApiError(400, 'Failed to create user');
+  if (!createdUser) {
+    throw new ApiError(400, 'Failed to create');
   }
   return createdUser;
 };

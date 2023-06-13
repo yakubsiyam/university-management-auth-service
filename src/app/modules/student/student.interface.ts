b95ff7e2589c7862b1836eacb1c3220d64bd02b3
yakubsiyam <itsyakubsiyam@gmail.com>
2023-06-13 18:14:46 +0600

    Update code

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,4 @@
+// import { InferSchemaType } from "mongoose";
+// import { StudentSchema } from "./student.model";
+
+// type User = InferSchemaType<typeof StudentSchema>
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -4,6 +4,9 @@ export type IUser = {
   id: string;
   role: string;
   password: string;
+  // student?: Types.ObjectId | IStudent,
+  // faculty?: Types.ObjectId | IFaculty;  future
+  // admin?: Type.ObjectId | IAdmin; future
 };
 
 export type UserModel = Model<IUser, Record<string, unknown>>;
